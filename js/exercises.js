7db8cfe7752301db5bec17dde41e79046f30bfde
cdelgado <=>
2024-09-01 19:17:45 -0300

    Agregado de nuevos ejercicios.

--- js/exercises.js
+++ js/exercises.js
@@ -181,6 +181,237 @@ let exercises = [
         "3_exercise_info": "Completá utilizando pronombres y/o <strong>am</strong>, <strong>is</strong>, o <strong>are</strong>",
         "4_words": ["Annie", "and", "I", 0, "friends.", 1, "hungry."],
         "5_possibleAnswers": [["am", "is", "are"], ["She's", "He's", "I'm", "You're", "We're", "They're"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 1,
+        "3_exercise_info": "Encontrá la respuesta más indicada para la pregunta",
+        "4_words": ["Where's the camera?", 0],
+        "5_possibleAnswers": [["London", "In your bag", "No, she's American"], ["No, I'm not", "Yes, you are", "My sister", "Black", "No, it's black", "Very well"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 2,
+        "3_exercise_info": "Encontrá la respuesta más indicada para la pregunta",
+        "4_words": ["Is your car blue?", 0],
+        "5_possibleAnswers": [["No, I'm not", "Yes, you are", "No, it's black"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 3,
+        "3_exercise_info": "Encontrá la respuesta más indicada para la pregunta",
+        "4_words": ["Is Kate from London?", 0],
+        "5_possibleAnswers": [["No, she's American", "Yes, you are", "London"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 4,
+        "3_exercise_info": "Encontrá la respuesta más indicada para la pregunta",
+        "4_words": ["Am I late?", 0, "Are you hungry?", 1],
+        "5_possibleAnswers": [["No, she's American", "Yes, you are", "No, I'm not"], ["No, I'm not", "Yes, you are", "Very well"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 5,
+        "3_exercise_info": "Encontrá la respuesta más indicada para la pregunta",
+        "4_words": ["Where's Amy from?", 0],
+        "5_possibleAnswers": [["No, she's American", "My sister", "London"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 6,
+        "3_exercise_info": "Encontrá la respuesta más indicada para la pregunta",
+        "4_words": ["What colour is your flag?", 0],
+        "5_possibleAnswers": [["No, she's American", "Black", "No, it's black"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 7,
+        "3_exercise_info": "Encontrá la respuesta más indicada para la pregunta",
+        "4_words": ["How is George?", 0, "Who's that woman?", 1],
+        "5_possibleAnswers": [["Very well", "No, I'm not", "In your bag"], ["Very well", "No, she's American", "My sister"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 8,
+        "3_exercise_info": "Hacé una pregunta con estas palabras",
+        "4_words": ["is / at home / your mother", 0],
+        "5_possibleAnswers": [["At home is your mother?", "Your mother is at home?", "Is your mother at home?"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 9,
+        "3_exercise_info": "Hacé una pregunta con estas palabras",
+        "4_words": ["your parents / are / well", 0],
+        "5_possibleAnswers": [["Well are your parents?", "Your parents are well?", "Are your parents well?"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 10,
+        "3_exercise_info": "Hacé una pregunta con estas palabras",
+        "4_words": ["interesting / is / your job", 0],
+        "5_possibleAnswers": [["Is interesting your job?", "Your job is interesting?", "Is your job interesting?"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 11,
+        "3_exercise_info": "Hacé una pregunta con estas palabras",
+        "4_words": ["the shops / are / open today", 0],
+        "5_possibleAnswers": [["Are the shops open today?", "Are open today the shops?", "The shops are open today?"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 12,
+        "3_exercise_info": "Hacé una pregunta con estas palabras",
+        "4_words": ["from / where / you / are", 0],
+        "5_possibleAnswers": [["From where are you?", "Are from where you?", "Where are you from?"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 13,
+        "3_exercise_info": "Hacé una pregunta con estas palabras",
+        "4_words": ["interested in sport / you / are", 0],
+        "5_possibleAnswers": [["You are interested in sport?", "Are you interested in sport?", "Interested in sport are you?"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 14,
+        "3_exercise_info": "Hacé una pregunta con estas palabras",
+        "4_words": ["is / near here / the station", 0],
+        "5_possibleAnswers": [["Is the station near here?", "The station near here is?", "Is near here the station?"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 15,
+        "3_exercise_info": "Hacé una pregunta con estas palabras",
+        "4_words": ["at school / are / your children", 0],
+        "5_possibleAnswers": [["Are at school your children?", "Are your children at school?", "At school your children are?"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 16,
+        "3_exercise_info": "Hacé una pregunta con estas palabras",
+        "4_words": ["you / are / late / why", 0],
+        "5_possibleAnswers": [["Why are you late?", "Why you are late?", "Whay late you are?"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 17,
+        "3_exercise_info": "Completá las preguntas con What..., Who..., Where... y How...",
+        "4_words": [0, 1, "your parents?"],
+        "5_notes": "Respuesta:&nbsp;<strong>They're very well</strong>.",
+        "5_possibleAnswers": [["What", "Who", "Where", "How", "Why"], ["is", "am", "are"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 18,
+        "3_exercise_info": "Completá las preguntas con What..., Who..., Where... y How...",
+        "4_words": [0, 1, "the bus station?"],
+        "5_notes": "Respuesta:&nbsp;<strong>At the end of the street</strong>.",
+        "5_possibleAnswers": [["What", "Who", "Where", "How", "Why"], ["is", "am", "are"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 19,
+        "3_exercise_info": "Completá las preguntas con What..., Who..., Where... y How...",
+        "4_words": [0, 1, 2, "your children?"],
+        "5_notes": "Respuesta:&nbsp;<strong>Five, six and ten</strong>.",
+        "5_possibleAnswers": [["What", "Who", "Where", "How", "Why"], ["old", "much", "long", "many", "colour"], ["is", "am", "are"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 20,
+        "3_exercise_info": "Completá las preguntas con What..., Who..., Where... y How...",
+        "4_words": [0, 1, 2, "these oranges?"],
+        "5_notes": "Respuesta:&nbsp;<strong>US$2.40 a kilo</strong>.",
+        "5_possibleAnswers": [["What", "Who", "Where", "How", "Why"], ["old", "much", "long", "many", "colour"], ["is", "am", "are"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 21,
+        "3_exercise_info": "Completá las preguntas con What..., Who..., Where... y How...",
+        "4_words": [0, 1, "your favourite sport?"],
+        "5_notes": "Respuesta:&nbsp;<strong>Skiing</strong>.",
+        "5_possibleAnswers": [["What", "Who", "Where", "How", "Why"], ["is", "am", "are"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 22,
+        "3_exercise_info": "Completá las preguntas con What..., Who..., Where... y How...",
+        "4_words": [0, 1, "the man in this photo?"],
+        "5_notes": "Respuesta:&nbsp;<strong>That's my father</strong>.",
+        "5_possibleAnswers": [["What", "Who", "Where", "How", "Why"], ["is", "am", "are"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 23,
+        "3_exercise_info": "Completá las preguntas con What..., Who..., Where... y How...",
+        "4_words": [0, 1, 2, "your new shoes?"],
+        "5_notes": "Respuesta:&nbsp;<strong>Black</strong>.",
+        "5_possibleAnswers": [["What", "Who", "Where", "How", "Why"], ["old", "much", "long", "many", "colour"], ["is", "am", "are"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 24,
+        "3_exercise_info": "Completá con respuestas tipo (Yes, I am / No, he isn't / etc.)",
+        "4_words": ["Are you married?", 0, 1, 2],
+        "5_possibleAnswers": [["No,", "Yes,"], ["I", "you", "he", "she", "it", "we", "they"], ["am", "am not", "is", "isn't", "are", "aren't"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 25,
+        "3_exercise_info": "Completá con respuestas tipo (Yes, I am / No, he isn't / etc.)",
+        "4_words": ["Is Paul thirsty?", 0, 1, 2],
+        "5_possibleAnswers": [["No,", "Yes,"], ["I", "you", "he", "she", "it", "we", "they"], ["am", "am not", "is", "isn't", "are", "aren't"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 26,
+        "3_exercise_info": "Completá con respuestas tipo (Yes, I am / No, he isn't / etc.)",
+        "4_words": ["Is it cloudy today?", 0, 1, 2],
+        "5_possibleAnswers": [["No,", "Yes,"], ["I", "you", "he", "she", "it", "we", "they"], ["am", "am not", "is", "isn't", "are", "aren't"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 27,
+        "3_exercise_info": "Completá con respuestas tipo (Yes, I am / No, he isn't / etc.)",
+        "4_words": ["Are your hands cold?", 0, 1, 2],
+        "5_possibleAnswers": [["No,", "Yes,"], ["I", "you", "he", "she", "it", "we", "they"], ["am", "am not", "is", "isn't", "are", "aren't"]]
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 2,
+        "3_exercise": 28,
+        "3_exercise_info": "Completá con respuestas tipo (Yes, I am / No, he isn't / etc.)",
+        "4_words": ["Are we teachers?", 0, 1, 2],
+        "5_possibleAnswers": [["No,", "Yes,"], ["I", "you", "he", "she", "it", "we", "they"], ["am", "am not", "is", "isn't", "are", "aren't"]]
     }
 ]
 
