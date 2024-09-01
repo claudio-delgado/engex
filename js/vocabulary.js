4d5af5b64198132493963fea46622bf569e6c170
cdelgado <=>
2024-09-01 19:17:09 -0300

    Agregado de vocabulario.

--- js/vocabulary.js
+++ js/vocabulary.js
@@ -27,12 +27,11 @@ let exercise_vocabulary = [
                         {"answer": "Es un adjetivo", "correct" : false}
                     ],
                     [
-                        {"answer": "Indica ubicación exacta", "correct" : true},
+                        {"answer": "Para ubicación exacta", "correct" : true},
+                        {"answer": "Es menos preciso que 'in' y 'on", "correct" : false},
                         {"answer": "Significa 'en'", "correct" : true},
                         {"answer": "Se usa para lugar y tiempo", "correct" : true},
-                        {"answer": "Es menos preciso que 'in' y 'on", "correct" : false},
-                        {"answer": "Significa 'encima'", "correct" : false},
-                        {"answer": "Es más preciso que 'in' y 'on", "correct" : true}
+                        {"answer": "Significa 'encima'", "correct" : false}
                     ]
                 ]
             },
@@ -54,5 +53,252 @@ let exercise_vocabulary = [
             }
         ],
         "5_translation": "Sarah ___ en casa."
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 1,
+        "3_exercise": 2,
+        "4_words": [
+            {
+                "1_token" : "I",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un nombre de persona", "correct" : false},
+                        {"answer": "Es un pronombre", "correct" : true},
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Es un adjetivo", "correct" : false}
+                    ],
+                    [
+                        {"answer": "Significa 'Y'", "correct" : false},
+                        {"answer": "Significa 'Yo'", "correct" : true},
+                        {"answer": "1era persona del singular", "correct" : true},
+                        {"answer": "Significa 'Vos'", "correct" : false}
+                    ]
+                ]
+            },
+            {
+                "1_token" : "and",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un sustantivo", "correct" : false},
+                        {"answer": "Es un artículo indeterminado", "correct" : false},
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Significa 'y'", "correct" : true},
+                        {"answer": "Significa 'o'", "correct" : false}
+                    ]
+                ]
+            },
+            {
+                "1_token" : "he",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Es un pronombre", "correct" : true},
+                        {"answer": "Es un adjetivo", "correct" : false},
+                        {"answer": "Es una preposición", "correct" : false},
+                        {"answer": "Es un adverbio", "correct" : false}
+                    ],
+                    [
+                        {"answer": "Significa 'él'", "correct" : true},
+                        {"answer": "Se usa sólo como sujeto", "correct" : true},
+                        {"answer": "Se usa como sujeto y objeto", "correct" : false},
+                        {"answer": "Tercera persona del singular", "correct" : true},
+                        {"answer": "Significa 'ella'", "correct" : false}
+                    ]
+                ]
+            },
+            {
+                "1_token" : "here",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Significa 'oir'", "correct" : false},
+                        {"answer": "Es un adjetivo", "correct" : false},
+                        {"answer": "Significa 'acá' o 'aquí'", "correct" : true}
+                    ],
+                    [
+                        {"answer": "Indica cercanía", "correct" : true},
+                        {"answer": "Significa 'allí' o 'allá'", "correct" : false},
+                        {"answer": "Indica lejanía", "correct" : false},
+                        {"answer": "Significa 'aquello'", "correct" : false}
+                    ]
+                ]
+            }
+        ],
+        "5_translation": "Yo ___ y él ___ acá."
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 1,
+        "3_exercise": 3,
+        "4_words": [
+            {
+                "1_token" : "You",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un nombre de persona", "correct" : false},
+                        {"answer": "Es un pronombre", "correct" : true},
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Es un posesivo", "correct" : false}
+                    ],
+                    [
+                        {"answer": "Significa 'Nosotros'", "correct" : false},
+                        {"answer": "Significa 'Yo'", "correct" : false},
+                        {"answer": "2da persona del singular", "correct" : true},
+                        {"answer": "Significa 'Vos'", "correct" : true}
+                    ],
+                    [
+                        {"answer": "Significa 'Ustedes'", "correct" : true},
+                        {"answer": "Significa 'Ellos'", "correct" : false},
+                        {"answer": "3era persona del singular", "correct" : false},
+                        {"answer": "2da persona del plural", "correct" : true}
+                    ]
+                ]
+            },
+            {
+                "1_token" : "my",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un sustantivo", "correct" : false},
+                        {"answer": "Es un artículo indeterminado", "correct" : false},
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Es un posesivo", "correct" : true},
+                        {"answer": "Es lo mismo que 'me'", "correct" : false}
+                    ],
+                    [
+                        {"answer": "Significa 'Yo'", "correct" : false},
+                        {"answer": "Significa 'Mi'", "correct" : true},
+                        {"answer": "Se usa cuando se dirigen a mí", "correct" : false},
+                        {"answer": "Es un pronombre de objeto", "correct" : false}
+                    ]
+                ]
+            },
+            {
+                "1_token" : "daughter",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Es un parentesco de familia", "correct" : true},
+                        {"answer": "Es un adjetivo", "correct" : false},
+                        {"answer": "Es una preposición", "correct" : false},
+                        {"answer": "Es un sustantivo", "correct" : true}
+                    ],
+                    [
+                        {"answer": "Significa 'hija'", "correct" : true},
+                        {"answer": "Es una palabra en plural", "correct" : false},
+                        {"answer": "Significa 'hermana'", "correct" : false},
+                        {"answer": "Es de género femenino", "correct" : true},
+                        {"answer": "Significa 'hijo'", "correct" : false}
+                    ]
+                ]
+            }
+        ],
+        "5_translation": "Vos ___ mi hija."
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 1,
+        "3_exercise": 4,
+        "4_words": [
+            {
+                "1_token" : "Joe",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Es un pronombre", "correct" : false},
+                        {"answer": "Es un adjetivo", "correct" : false},
+                        {"answer": "Es un nombre de persona", "correct" : true},
+                        {"answer": "Es un sustantivo", "correct" : true}
+                    ],
+                    [
+                        {"answer": "Es de género masculino", "correct" : true},
+                        {"answer": "Es de género femenino", "correct" : false}
+                    ]
+                ]
+            },
+            {
+                "1_token" : "and",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un sustantivo", "correct" : false},
+                        {"answer": "Es un artículo indeterminado", "correct" : false},
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Significa 'y'", "correct" : true},
+                        {"answer": "Significa 'o'", "correct" : false}
+                    ]
+                ]
+            },
+            {
+                "1_token" : "I",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un nombre de persona", "correct" : false},
+                        {"answer": "Es un pronombre", "correct" : true},
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Es un adjetivo", "correct" : false}
+                    ],
+                    [
+                        {"answer": "Significa 'Y'", "correct" : false},
+                        {"answer": "Significa 'Yo'", "correct" : true},
+                        {"answer": "1era persona del singular", "correct" : true},
+                        {"answer": "Significa 'Vos'", "correct" : false}
+                    ]
+                ]
+            },
+            {
+                "1_token" : "friends",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Es una palabra en singular", "correct" : false},
+                        {"answer": "Es un adjetivo", "correct" : false},
+                        {"answer": "Significa 'amigas' o 'amigos'", "correct" : true}
+                    ],
+                    [
+                        {"answer": "Es una palabra en plural", "correct" : true},
+                        {"answer": "Significa 'enemigo'", "correct" : false},
+                        {"answer": "Se escribe igual en singular y plural", "correct" : false},
+                        {"answer": "Sólo es de género masculino", "correct" : false}
+                    ]
+                ]
+            }
+        ],
+        "5_translation": "Joe y yo ___ amigos."
+    },
+    {
+        "1_module": 1,
+        "2_submodule": 1,
+        "3_exercise": 5,
+        "4_words": [
+            {
+                "1_token" : "Paul",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un verbo", "correct" : false},
+                        {"answer": "Es un pronombre", "correct" : false},
+                        {"answer": "Es un adjetivo", "correct" : false},
+                        {"answer": "Es un nombre de persona", "correct" : true},
+                        {"answer": "Es un sustantivo", "correct" : true}
+                    ],
+                    [
+                        {"answer": "Es de género masculino", "correct" : true},
+                        {"answer": "Es de género femenino", "correct" : false}
+                    ]
+                ]
+            },
+            {
+                "1_token" : "thirsty",
+                "2_translations" : [
+                    [
+                        {"answer": "Es un sustantivo", "correct" : false},
+                        {"answer": "Es un adjetivo", "correct" : true},
+                        {"answer": "Significa 30", "correct" : false},
+                        {"answer": "Significa 'sediento'", "correct" : true},
+                        {"answer": "Significa 'hambriento'", "correct" : false}
+                    ]
+                ]
+            }
+        ],
+        "5_translation": "Paul ___ sediento. (tiene sed)"
     }
 ]
